dc26004794f6b8fbb55d364bae134104cf003d21
Oleksandr Obukh <obukh.aleksandr@gmail.com>
2022-08-02 23:44:48 +0300

    Change add deploy Jenkinsfile

---
+++ Jenkinsfile_deploy.groovy
@@ -0,0 +1,30 @@
+#!groovy
+pipeline {
+    environment {
+        imagename = "ptk2006/spring-petclinic"
+        dev_instance = "tf-target-01.westeurope.cloudapp.azure.com"
+        qa_instance = "tf-target-02.westeurope.cloudapp.azure.com"        
+    }
+    agent any
+    stages {     
+        stage("Deploy artifacts") {
+            steps {
+                script {
+                    if (env.TARGET_ENVIRONMENT == "DEV") {
+                        echo "Deploying to DEV environment"
+                        INSTANCE = ${dev_instance}
+                    } else if (env.TARGET_ENVIRONMENT == "QA") {
+                        echo "Deploying to QA environment"
+                        INSTANCE = ${qa_instance}
+                    }
+
+                    echo "Target instance IP: ${INSTANCE}"
+
+                    withCredentials([sshUserPrivateKey(credentialsId: 'tls_private_key', keyFileVariable: 'SSH_KEY', passphraseVariable: '', usernameVariable: 'SSH_USER')]) {
+                        sh 'ssh -oStrictHostKeyChecking=no -i $SSH_KEY $SSH_USER@$INSTANCE docker run -d -p 80:8080 $imagename:$BUILD_VERSION'
+                    }
+                }
+            }
+        }
+    }
+}
