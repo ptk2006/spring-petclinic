53afcbc16df404b5fc6af6f371ec0a3d0a548101
Oleksandr Obukh <obukh.aleksandr@gmail.com>
2022-08-03 13:12:07 +0300

    Change deploy Jenkinsfile

--- Jenkinsfile_deploy.groovy
+++ Jenkinsfile_deploy.groovy
@@ -14,7 +14,6 @@ pipeline {
                     if (env.TARGET_ENVIRONMENT == "DEV") {
                         echo "Deploying to DEV environment"
                         INSTANCE = env.dev_instance
-                        curl_command = "$curl_command_template$env.INSTANCE"
                     } else if (env.TARGET_ENVIRONMENT == "QA") {
                         echo "Deploying to QA environment"
                         INSTANCE = env.qa_instance
@@ -31,6 +30,7 @@ pipeline {
         stage("Check deploying") {
             steps {
                 script {
+                    curl_command = "$curl_command_template$INSTANCE"
                     echo "$curl_command"
                     if (env.curl_command == '200') {
                         echo 'I only execute on the master branch'
