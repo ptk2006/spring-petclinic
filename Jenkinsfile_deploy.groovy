aed03c01f850dee978c0956137bb15e81c6eea62
Oleksandr Obukh <obukh.aleksandr@gmail.com>
2022-08-03 15:22:51 +0300

    Change deploy Jenkinsfile

--- Jenkinsfile_deploy.groovy
+++ Jenkinsfile_deploy.groovy
@@ -22,8 +22,8 @@ pipeline {
                     echo "Target instance IP: $INSTANCE"
 
                     withCredentials([sshUserPrivateKey(credentialsId: 'tls_private_key', keyFileVariable: 'SSH_KEY', usernameVariable: 'SSH_USER')]) {
-                        sh 'for id in $(docker ps -q); do if [[ $(docker port "${id}") == *":80"* ]]; then docker stop "${id}"; fi; done'
-                        sh "sleep 10; ssh -oStrictHostKeyChecking=no $SSH_USER@$INSTANCE -i $SSH_KEY docker run -d -p 80:8080 $imagename:$BUILD_VERSION"
+                        sh 'for id in $(docker ps -q); do if [[ $(docker port "${id}") == *":80"* ]]; then sudo docker stop "${id}"; fi; done'
+                        sh "ssh -oStrictHostKeyChecking=no $SSH_USER@$INSTANCE -i $SSH_KEY docker run -d -p 80:8080 $imagename:$BUILD_VERSION"
                     }
                 }
             }
