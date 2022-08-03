1e9ca0b33c348df3bcf5aacc0c2d3a11c3ced525
Oleksandr Obukh <obukh.aleksandr@gmail.com>
2022-08-03 16:52:18 +0300

    Change deploy Jenkinsfile

--- Jenkinsfile_deploy.groovy
+++ Jenkinsfile_deploy.groovy
@@ -2,6 +2,7 @@
 pipeline {
     environment {
         imagename = "ptk2006/spring-petclinic"
+        containername = "petclinic"
         dev_instance = "tf-target-01.westeurope.cloudapp.azure.com"
         qa_instance = "tf-target-02.westeurope.cloudapp.azure.com"
         curl_command_template = "curl -o /dev/null -s -w '%{http_code}\n' http://"        
@@ -22,14 +23,8 @@ pipeline {
                     echo "Target instance IP: $INSTANCE"
 
                     withCredentials([sshUserPrivateKey(credentialsId: 'tls_private_key', keyFileVariable: 'SSH_KEY', usernameVariable: 'SSH_USER')]) {
-                        sh '''
-                        ssh -oStrictHostKeyChecking=no $SSH_USER@$INSTANCE -i $SSH_KEY << ENDSSH
-                        echo "hello world"
-                        for id in $(docker ps -q); do if [[ $(docker port "${id}") == *":80"* ]]; then sudo docker stop "${id}"; fi; done
-ENDSSH
-'''
-                        // sh ('ssh -oStrictHostKeyChecking=no $SSH_USER@$INSTANCE -i $SSH_KEY for id in $(docker ps -q); do if [[ $(docker port "${id}") == *"80"* ]]; then sudo docker stop "${id}"; fi; done')
-                        sh "ssh -oStrictHostKeyChecking=no $SSH_USER@$INSTANCE -i $SSH_KEY docker run -d -p 80:8080 $imagename:$BUILD_VERSION"
+                        sh "ssh -oStrictHostKeyChecking=no $SSH_USER@$INSTANCE -i $SSH_KEY docker rm -f $petclinic || true"
+                        sh "ssh -oStrictHostKeyChecking=no $SSH_USER@$INSTANCE -i $SSH_KEY docker run -- name $petclinic -d -p 80:8080 $imagename:$BUILD_VERSION"
                     }
                 }
             }
