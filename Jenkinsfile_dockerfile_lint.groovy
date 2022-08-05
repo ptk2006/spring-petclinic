3bace9b7bcdc894f1f532d67e6e8efb43021ac4a
Oleksandr Obukh <obukh.aleksandr@gmail.com>
2022-08-06 00:25:19 +0300

    Add linter task

---
+++ Jenkinsfile_dockerfile_lint.groovy
@@ -0,0 +1,29 @@
+#!groovy
+pipeline {
+    environment {
+        imagename = "ptk2006/spring-petclinic"
+    }
+    agent none
+    stages {     
+        stage("Clone feature") {
+            steps {
+                git branch: 'feature', credentialsId: 'Gitlab_key', url: 'git@github.com:ptk2006/spring-petclinic.git'
+            }
+        }
+        stage("TestDockerfile") {
+            steps {
+                script {
+                    def lintResult = sh returnStdout: true, script: 'docker run --rm -i hadolint/hadolint < Dockerfile'
+                    if (lintResult.trim() == '') {
+                        println 'Lint finished with no errors'
+                    } else {
+                        println 'Error found in Lint'
+                        println "${lintResult}"
+                        currentBuild.result = 'UNSTABLE'
+                    }
+                }
+            }
+        }
+
+    }
+}
